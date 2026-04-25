9cb63be51fc28229a75b30af6ba264dc827e0ab2
Abdelhak AMZIL <abdelhakamzil.2000@gmail.com>
2026-04-25 12:49:22 +0100

    feat(notification): Implement the notification micro service

---
+++ services/notification/src/main/java/com/abdo/ecommerce/kafka/payment/PaymentMethod.java
@@ -0,0 +1,10 @@
+package com.abdo.ecommerce.kafka.payment;
+
+public enum PaymentMethod {
+
+    PAYPAL,
+    CREDIT_CREDIT,
+    VISA,
+    MASTER_CARD,
+    BITCOIN
+}
---
+++ services/notification/src/main/java/com/abdo/ecommerce/notification/NotificationRepository.java
@@ -0,0 +1,7 @@
+package com.abdo.ecommerce.notification;
+
+import com.abdo.ecommerce.kafka.payment.PaymentConfirmation;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface NotificationRepository extends MongoRepository<Notification, String> {
+}
